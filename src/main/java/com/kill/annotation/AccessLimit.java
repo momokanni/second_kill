d4f43b879c93f9d3c09fd90e3c539bacbd19daf3
momokanni <740949744@qq.com>
2018-08-08 11:52:12 +0800

    配置项

--- src/main/java/com/kill/annotation/AccessLimit.java
+++ src/main/java/com/kill/annotation/AccessLimit.java
@@ -5,6 +5,14 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * class_name: AccessLimit
+ * package: com.kill.annotation
+ * describe: 限制访问次数注解 和 拦截器：AccessLimitInterceptor 配合使用
+ * creat_user: sl
+ * creat_date: 2018/8/8
+ * creat_time: 11:50
+ **/
 @Target({METHOD})
 @Retention(RUNTIME)
 public @interface AccessLimit {
